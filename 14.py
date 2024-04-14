485da48e0da893a1fe9260d84eef8374352ad71c
SyedNafeesOP <syednafeesop.2000@gmail.com>
2024-04-14 09:51:57 -0700

    Here we dicuss about recursion

---
+++ 14.py
@@ -0,0 +1,26 @@
+# Here in this file we learn about recursion
+
+# Recursion is simply defined as when a function calls itself repeatedly
+
+def show(n):
+    if n==0: # This is called base casae
+        return
+    print(n)
+    show(n-1)  
+show(5)    
+
+
+# factorial example
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n* factorial(n-1)
+
+print(factorial(5))    
+print(factorial(4))    
+print(factorial(3))    
+    
+
+    
