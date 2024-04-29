4e04e5ff47bd36d9937bcac4583ecac3551db4ad
SyedNafeesOP <syednafeesop.2000@gmail.com>
2024-04-29 01:52:08 -0700

    Inheritance

--- final.py
+++ final.py
@@ -23,3 +23,37 @@ print(acc1.acc)
 #print(acc1.__pas)
 
 acc1.reset()
+
+
+# inheritance
+
+class Dreamcar():
+    def __init__(self, fuel, horsepower, brand):
+        self.fuel = fuel
+        self.horsepower = horsepower
+        self.brand = brand
+
+    @staticmethod
+    def car():
+        print("NISSAN GTR")
+
+    def info(self):
+        print(f"Brand: {self.brand}, Fuel: {self.fuel}, Horsepower: {self.horsepower}")
+
+
+class Porsche(Dreamcar):
+    def __init__(self, name, fuel, horsepower):
+        super().__init__(fuel, horsepower, "Porsche")
+        self.name = name
+
+
+car1 = Dreamcar("petrol", 600, "Bugatti")
+car2 = Porsche("2023 Porsche 911", "petrol", 450)
+
+car1.car()
+car1.info()
+
+car2.car()
+car2.info()
+
+
